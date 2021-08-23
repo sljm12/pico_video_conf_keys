871178132d080f0d7ce461efe3160b0503205f40
stephenleejm <stephenleejm@gmail.com>
2021-08-23 09:39:11 +0800

    Added bluejeans keys

--- vtc_keys.py
+++ vtc_keys.py
@@ -35,3 +35,40 @@ class Zoom:
             print("Google Meet camera pressed")
             self.kbd.send(Keycode.ALT, Keycode.V)
         return t
+    
+    
+class ZoomMac:
+    def __init__(self, kbd):
+        '''
+        kbd a AdaFruit HID Keyboard instance
+        '''
+        self.kbd = kbd
+    def mute(self):
+        def t():
+            print("Zoom Mac mute/unmute pressed")
+            self.kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.A)
+        return t
+        
+    def camera(self):
+        def t():
+            print("Zoom Mac camera pressed")
+            self.kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.V)
+        return t
+    
+class BlueJeans:
+    def __init__(self, kbd):
+        '''
+        kbd a AdaFruit HID Keyboard instance
+        '''
+        self.kbd = kbd
+    def mute(self):
+        def t():
+            print("Blue Jeans mute/unmute pressed")
+            self.kbd.send(Keycode.M)
+        return t
+        
+    def camera(self):
+        def t():
+            print("Blue Jeans Video pressed")
+            self.kbd.send(Keycode.V)
+        return t
